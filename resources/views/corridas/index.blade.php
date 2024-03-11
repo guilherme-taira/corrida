70042a02f02c6850250dce720d11ff41ad7df3c2
guilherme-taira <54184559+guilherme-taira@users.noreply.github.com>
2024-03-10 23:45:31 -0300

    este commit adiciona css

--- resources/views/corridas/index.blade.php
+++ resources/views/corridas/index.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.corridas')
 
 @section('conteudo')
-
-    <div class="containerfoto">
-        <div class="page-heading">
+    <div class="page-heading">
+        <img  class="page-headingbg" src="{{asset('images/heading-bg.jpg')}}" alt="">
+        <div class="containerFoto">
             <div class="row">
                 <div class="col-lg-12">
                     <div class="header-text">
@@ -47,12 +47,12 @@
                         @if (Auth::user())
                             <td class="link-success text-center"><a class="btn btn-warning"
                                     href="{{ route('editar', ['id' => $corrida->id]) }}">Editar</a></td>
-                            <form method="POST" action="{{ route('corridas.destroy', ['corrida' => $corrida->id]) }}">
-                                @csrf
-                                @method('delete')
-                                <td class="link-success  text-center">
-                                    <button type="submit" class="btn btn-danger">Apagar</button>
-                                </td>
+                            <form method="POST" action="{{route('corridas.destroy',['corrida' => $corrida->id])}}">
+                            @csrf
+                            @method('delete')
+                            <td class="link-success  text-center">
+                                <button type="submit" class="btn btn-danger">Apagar</button>
+                            </td>
                             </form>
                         @endif
                     </tr>
